dc7f87fec8312cb8cb330a2e51f5ac707d03619f
Mayur Kachhadiya <mayurkachhadiya373@gmail.com>
2022-06-21 18:40:12 +0530

    FOOD

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+// import GalleryReact from "./component/TabMenu/GalleryReact";
+import Todo from './component/todoProject/Todo';
+
+const App = () => {
+  return (
+    <>
+    <Todo/>
+     {/* <GalleryReact /> */}
+    </>
+  )
+}
+
+export default App
